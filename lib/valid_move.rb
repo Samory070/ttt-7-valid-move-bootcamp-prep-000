58300da79e25cb704e9f92c68edb0eb4c9c6f8c0
samory harris <samory070@gmail.com>
2018-03-09 19:56:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,4 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-def valid_move?(arr, index)
+def position_taken?(arr, index)
   (arr[index] == '' || arr[index] == ' ' || arr[index] == nil) ? false : true
 end
